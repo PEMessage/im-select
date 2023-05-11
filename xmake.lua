cd2f3d20909f8ac6b7b36962a4f94148bcee1b82
PEMessage <1165739182@qq.com>
2023-05-11 19:35:37 +0800

    modified:   xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,6 @@ add_rules("mode.debug", "mode.release")
 target("im-select-imm")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_files("src/stdafx.cpp")
     
     add_includedirs("inc/")
  
